2646aa4db281d75d468dd92a59b358c20be96d41
Cris01000011 <168916184+Cris01000011@users.noreply.github.com>
2024-07-04 22:44:48 -0600

    test h2

---
+++ src/test/java/com/example/codelab_h2/CodelabH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codelab_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelabH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
